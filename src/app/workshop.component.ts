ac4154cb9f272e3e0929278488926ddf0390e586
angular-cli <angular-cli@angular.io>
2016-05-16 09:12:36 -0500

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/workshop.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'workshop-app',
+  templateUrl: 'workshop.component.html',
+  styleUrls: ['workshop.component.css']
+})
+export class WorkshopAppComponent {
+  title = 'workshop works!';
+}
